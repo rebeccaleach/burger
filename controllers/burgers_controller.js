70242c5220adbcf6c40bb1d64b47170668b30fee
Rebecca Leach <flytrapcat@gmail.com>
2016-11-06 22:44:35 -0600

    initial commit

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,43 @@
+// routes
+
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger.js');
+
+router.get('/', function (req, res) {
+	res.redirect('/burgers');
+});
+
+router.get('/burgers', function (req, res) {
+	burger.all(function (data) {
+		var hbsObject = { burgers: data };
+		console.log(hbsObject);
+		res.render('index', hbsObject);
+	});
+});
+
+router.post('/burgers/create', function (req, res) {
+	burger.create(['name', 'eaten'], [req.body.name, req.body.eaten], function () {
+		res.redirect('/burgers');
+	});
+});
+
+router.put('/burgers/update/:id', function (req, res) {
+	var condition = 'id = ' + req.params.id;
+
+	console.log('condition', condition);
+
+	burger.update({ eaten: req.body.eaten }, condition, function () {
+		res.redirect('/burgers');
+	});
+});
+
+router.delete('/burgers/delete/:id', function (req, res) {
+	var condition = 'id = ' + req.params.id;
+
+	burger.delete(condition, function () {
+		res.redirect('/burgers');
+	});
+});
+
+module.exports = router;
